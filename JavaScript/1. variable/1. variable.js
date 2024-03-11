f61c79485175ddd9c7971c74218711727614b96a
INSEONG-PC\정인성 <twin2688@naver.com>
2024-03-11 22:32:13 +0900

    자바스크립트 : 기초 공부 변수파트

---
+++ JavaScript/1. variable/1. variable.js
@@ -0,0 +1,13 @@
+// 변수 : 값을 저장하는 공간 , 자료를 저장할 수 있는 이름이 주어진 기억 장소
+let a = 0;
+console.log(a); // 0
+
+a = 1; // 재할당
+console.log(a);
+
+let b; // 선언
+
+console.log(b); // 선언만 할 경우(값을 할당하기 전) undefined가 나오게 된다.
+
+b = 2;
+console.log(b); // 2
---
+++ JavaScript/1. variable/2. naming.js
@@ -0,0 +1,29 @@
+/** 변수 규칙!
+ *  라틴문자(0-9,a-z,A-Z)
+ *  대소문자를 구분함
+ *  추천 : camelCase (likeThis) ✅
+ *  한국어 ❌
+ *  예약어 ❌
+ *  숫자로 시작 ❌
+ *  특수문자 ❌ ( _ $ 두가지는 예외 ✅)
+ *  이모지 ❌
+ *  여러개의 변수를 1,2,3 숫자로 구분 ❌
+ */
+
+// 나쁜 예제 😡
+let num = 20;
+
+// 좋은 예제 😁
+let myAge = 25;
+
+// 나쁜 예제 😡
+let audio1;
+let audio2;
+
+// 좋은 예제 😁
+let backgroundAudio;
+let windAudio;
+
+// 꿀팁 💫 → audio만 쳐도 어떤 audio인지 예시가 나온다.
+let audioBackground;
+let audioWind;
