05c72fec675f0dba4785bce0e23d3d0b13fa2515
INSEONG-PC\정인성 <twin2688@naver.com>
2024-05-03 21:48:00 +0900

    알고리즘 : 큐 & 스택

---
+++ Algorithm/stack.js
@@ -0,0 +1,15 @@
+let stack = [];
+
+// 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제 - 삽입(1) - 삽입(4) - 삭제()
+stack.push(5);
+stack.push(2);
+stack.push(3);
+stack.push(7);
+stack.pop();
+stack.push(1);
+stack.push(4);
+stack.pop();
+
+let reversed = stack.slice().reverse();
+console.log(reversed);
+console.log(stack);
