6c4b1b57195d691cc284ec3433c46091b96cebe1
INSEONG-PC\정인성 <twin2688@naver.com>
2024-05-18 12:43:38 +0900

    알고리즘 : 버블정렬, 선택정렬, 삽입정렬

---
+++ Algorithm/sort/InsertionSort.js
@@ -0,0 +1,15 @@
+// 삽입 정렬 - Insertion Sort
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i; j > 0; j--) {
+      // 인덱스 j부터 1까지 1씩 감소하며 왼쪽 요소를 검사
+      if (arr[j] < arr[j - 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+      } else {
+        break;
+      }
+    }
+  }
+}
