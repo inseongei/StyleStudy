c1727355d4db14d42836739d5d15485ab1aff2cb
INSEONG-PC\정인성 <twin2688@naver.com>
2024-06-06 21:22:16 +0900

    리엑트 : state의 불변성을 유지하면서 추가, 삭제 , 수정

--- React/basic-app/src/index.js
+++ React/basic-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppProducts from "./AppProducts";
+import AppMentors from "./AppMentors";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppProducts />);
+root.render(<AppMentors />);
