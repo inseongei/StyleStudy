01b6fbbbf39eaede83221af77ad684ea7a5a62f4
INSEONG-PC\정인성 <twin2688@naver.com>
2024-06-08 17:24:13 +0900

    리엑트 : Form 태그 , useReducer , immer

--- React/basic-app/src/index.js
+++ React/basic-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppMentors from "./AppMentors";
+import AppForm from "./AppFrom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppMentors />);
+root.render(<AppForm />);
