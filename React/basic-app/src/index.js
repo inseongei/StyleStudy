332738444640de402fc09e8444138cfb9110fb24
INSEONG-PC\정인성 <twin2688@naver.com>
2024-06-21 22:10:14 +0900

    리엑트 : 컴포넌트 장점 & 예시

--- React/basic-app/src/index.js
+++ React/basic-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppForm from "./AppFrom";
+import AppCard from "./AppCard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppForm />);
+root.render(<AppCard />);
