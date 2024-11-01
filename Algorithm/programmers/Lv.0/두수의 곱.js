193cf954f269e33669b0534b386a4038a2ab8cff
inseongei <twin2688@naver.com>
2024-11-01 20:02:49 +0900

    Day 1 - 사칙 연산 문제 풀이

---
+++ Algorithm/programmers/Lv.0/두수의 곱.js
@@ -0,0 +1,7 @@
+/* 00:20 / 10:00 */
+
+function solution(num1, num2) {
+  return num1 * num2;
+}
+
+// 1. 매개변수로 주어지는 num1과 num2를 곱해주면 된다.
---
+++ Algorithm/programmers/Lv.0/몫 구하기.js
@@ -0,0 +1,8 @@
+/* 00:13 / 10:00 */
+
+function solution(num1, num2) {
+  return Math.floor(num1 / num2);
+}
+
+// 1. 주어지는 매개변수 num1과 num2를 받아 나눠준다.
+// 2. Math.floor 메소드를 사용해 소수점을 버려준다.
