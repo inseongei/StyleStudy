45e4a8d0c0dc9616a0796b2a224883cb93941635
inseongei <twin2688@naver.com>
2024-11-01 19:02:48 +0900

    테스트 폴더 정리

--- Test/3.8-unit-basic/src/async.js
+++
@@ -1,8 +0,0 @@
-function fetchProduct(error) {
-  if (error === "error") {
-    return Promise.reject("network Error");
-  }
-  return Promise.resolve({ item: "Milk", price: 200 });
-}
-
-module.exports = fetchProduct;
