086b61d78f3f4471db3fd9f7e82e6066df881285
inseongei <twin2688@naver.com>
2025-01-15 22:43:50 +0900

    리엑트 네이티브 : 핵심 구성요소 공부

--- React-Native/React Native Course/CoreComponet/App.js
+++ React-Native/React Native Course/CoreComponet/App.js
@@ -1,27 +1,11 @@
-import { View, Button, Modal, Text } from "react-native";
-import { useState } from "react";
+import { View } from "react-native";
+import Greet from "./components/Greet";
 
 export default function App() {
-  const [isModal, setIsModal] = useState(false);
-
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button title={"Press"} onPress={() => setIsModal(true)} color={"blue"} />
-      <Modal
-        visible={isModal}
-        onRequestClose={() => setIsModal(false)}
-        animationType="slide"
-        presentationStyle="pageSheet"
-      >
-        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
-          <Text>Modal Content</Text>
-          <Button
-            title="Close"
-            color="midnightblue"
-            onPress={() => setIsModal(false)}
-          />
-        </View>
-      </Modal>
+      <Greet name="inseong" />
+      <Greet name="Jung inseong" />
     </View>
   );
 }
